6f57cb2e84fab5b22ef9215b60e08f222a51fd85
Anna Headley <anna.headley@gmail.com>
2020-04-07 16:21:32 -0400

    Rewrite interface for query service
    Co-authored-by: Anna Headley <anna.headley@gmail.com>

---
+++ app/services/orcid_api/query.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+# results = OrcidApi::Query.institution
+module OrcidApi
+  class Query
+    def self.institution(affiliation: nil, grid: nil, ringgold: nil)
+      new(affiliation: affiliation, grid: grid, ringgold: ringgold).search_institution
+    end
+
+    def self.affiliation(affiliation: nil)
+      new(affiliation: affiliation).search_affiliation
+    end
+
+    def self.grid(grid: nil)
+      new(grid: grid).search_grid
+    end
+
+    def self.ringgold(ringgold: nil)
+      new(ringgold: ringgold).search_ringgold
+    end
+
+    # the number of rows to retrieve at a time
+    PAGE_SIZE = 1000
+    attr_reader :affiliation, :grid, :ringgold
+
+    def initialize(affiliation: nil, grid: nil, ringgold: nil)
+      @affiliation = affiliation || "Princeton University"
+      @grid = grid || "grid.16750.35"
+      @ringgold = ringgold || "6740"
+    end
+
+    def search_institution
+      fielded_search(
+        "affiliation-org-name" => affiliation,
+        "grid-org-id" => grid,
+        "ringgold-org-id" => ringgold
+      )
+    end
+
+    def search_affiliation
+      fielded_search("affiliation-org-name" => affiliation)
+    end
+
+    def search_grid
+      fielded_search("grid-org-id" => grid)
+    end
+
+    def search_ringgold
+      fielded_search("ringgold-org-id" => ringgold)
+    end
+
+    private
+
+      def fielded_search(fields)
+        results = []
+        start = 0
+        api_instance = SwaggerClient::DevelopmentMemberAPIV30Api.new
+
+        loop do
+          opts = {
+            q: fielded_query(fields),
+            rows: PAGE_SIZE,
+            start: start
+          }
+
+          # Search records
+          result = api_instance.search_by_queryv3(opts)
+          results.push(*result.result)
+          start += PAGE_SIZE
+          break if start >= result.num_found
+        end
+        results
+      end
+
+      def fielded_query(fields)
+        fields.to_a.map do |tuple|
+          "#{tuple.first}:(\"#{tuple.last}\")"
+        end.join(" OR ")
+      end
+  end
+end
--- spec/services/orcid_api/query_spec.rb
+++ spec/services/orcid_api/query_spec.rb
@@ -1,22 +1,24 @@
 # frozen_string_literal: true
 require "rails_helper"
 
-RSpec.describe QueryService do
-  let(:qs) { described_class.new(page_size: 3) }
+RSpec.describe OrcidApi::Query do
+  before do
+    stub_const("OrcidApi::Query::PAGE_SIZE", 3)
+  end
 
-  describe "#search_institution" do
+  describe ".institution" do
     let(:page1) { Rails.root.join("spec", "fixtures", "search_institution_rows_0-2.json") }
     it "executes a search with default parameters, navigating pagination, to return an array of orcid models" do
       stub_request(:get, "https://pub.orcid.org/v3.0/search?q=affiliation-org-name:(%22Princeton%20University%22)%20OR%20grid-org-id:(%22grid.16750.35%22)%20OR%20ringgold-org-id:(%226740%22)&rows=3&start=0")
         .to_return(status: 200, body: page1, headers: {})
 
-      results = qs.search_institution
+      results = described_class.institution
       expect(results.count).to eq 3
       expect(results.first.orcid_identifier.path).to eq "0000-0002-8471-7302"
     end
   end
 
-  describe "#search_affiliation" do
+  describe ".search_affiliation" do
     let(:page1) { Rails.root.join("spec", "fixtures", "search_affiliation_rows_0-2.json") }
     let(:page2) { Rails.root.join("spec", "fixtures", "search_affiliation_rows_3-5.json") }
 
@@ -46,13 +48,13 @@ RSpec.describe QueryService do
     end
 
     it "accepts a string and executes a search, navigating pagination, to return an array of orcid models" do
-      results = qs.search_affiliation("Princeton University")
+      results = described_class.affiliation
       expect(results.count).to eq 6
       expect(results.first.orcid_identifier.path).to eq "0000-0003-4571-9046"
     end
   end
 
-  describe "#search_grid" do
+  describe ".search_grid" do
     let(:page1) { Rails.root.join("spec", "fixtures", "search_grid_rows_0-2.json") }
     let(:page2) { Rails.root.join("spec", "fixtures", "search_grid_rows_3-4.json") }
 
@@ -64,13 +66,13 @@ RSpec.describe QueryService do
     end
 
     it "accepts a string and executes a search, navigating pagination, to return an array of orcid models" do
-      results = qs.search_grid("grid.16750.35")
+      results = described_class.grid
       expect(results.count).to eq 5
       expect(results.first.orcid_identifier.path).to eq "0000-0001-7864-0364"
     end
   end
 
-  describe "#search_ringgold" do
+  describe ".search_ringgold" do
     let(:page1) { Rails.root.join("spec", "fixtures", "search_ringgold_rows_0-2.json") }
     let(:page2) { Rails.root.join("spec", "fixtures", "search_ringgold_row_3.json") }
 
@@ -82,7 +84,7 @@ RSpec.describe QueryService do
     end
 
     it "accepts a string and executes a search, navigating pagination, to return an array of orcid models" do
-      results = qs.search_ringgold("6740")
+      results = described_class.ringgold
       expect(results.count).to eq 4
       expect(results.first.orcid_identifier.path).to eq "0000-0002-2525-2912"
     end
